2d2d0aeca76957b0a94429534a74880895e0bd04
Elyo102 <fire102.shits@gmail.com>
2026-08-17 22:22:13 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ messaging.onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'ds102-shell-v44';
+const CACHE_NAME = 'ds102-shell-v45';
 const SHELL_FILES = [
   './',
   './index.html',
