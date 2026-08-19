0ec6513a4ae6fac4d33275a53a25fb931187d1ea
Elyo102 <fire102.shits@gmail.com>
2026-08-19 12:45:16 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -88,7 +88,7 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-const CACHE_NAME = 'ds102-shell-v49';
+const CACHE_NAME = 'ds102-shell-v50';
 const SHELL_FILES = [
   './',
   './index.html',
